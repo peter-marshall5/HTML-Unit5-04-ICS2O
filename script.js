c10e0bb076acaca12b89fe791cf9f717b531f21c
Peter Marshall <peter.marshall@ocsbstudent.ca>
2021-02-12 14:34:27 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// Get input
+const input = document.getElementById('text')
+// Get button
+const button = document.getElementById('submit')
+
+// Take value out of text box and alert it
+function onclick() {
+  // Show alert message
+  alert(input.value)
+  // Clear value of input
+  input.value = ''
+}
+
+// Run 'onclick' when button is clicked
+button.addEventListener('click', onclick)
